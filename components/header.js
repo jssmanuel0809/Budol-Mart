d1c306ea91caad503510ad535c10f65a400a5b06
jemima <88172123+jssmanuel0809@users.noreply.github.com>
2023-11-29 14:36:20 +0800

    home page

---
+++ components/header.js
@@ -0,0 +1,93 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+    <head>
+        <style>
+            header {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                background-color: #F5E6CA;
+                padding-left: 20px;
+                padding-right: 20px;
+                height: 8vh;
+                z-index: 1;
+            }
+            .header{
+                width: 100%;
+                position: fixed;        
+                overflow: hidden;
+                top: 0;
+            }
+            .header-content{
+                display: flex;
+                justify-content: space-between;
+                align-items: cent
+            }
+            .logo{
+                font-family: 'Amarante', serif;
+                color: #3D4C6C;
+                text-decoration: none;
+                letter-spacing: 10px;
+                font-size: 40px;
+                margin-right: 10px;
+            }
+            input[name="search"]{
+                width: 418px;
+                border: 2px solid #3D4C6C;
+                border-radius: 25px;
+                background-color: #F8EFEF;
+                padding: 8px 20px;
+                margin: 8px 0;
+            }
+            .nav_links a {
+                font-family: 'Amarante', serif;
+                color: #3D4C6C;
+                text-decoration: none;
+            }
+            .nav_links {
+                list-style: none;
+                display: flex;
+                font-size: 26px;
+            }
+            .nav_links li {
+                padding: 5px 20px;
+            }
+            
+            .header-content .user{
+                background-color: #CFA180;
+                color: #F5E6CA;
+                text-decoration: none;
+                font-family: 'Amarante', serif;
+                font-size: 26px;
+                padding: 5px;
+                padding-left: 25px;
+                padding-right: 25px;
+            }
+        </style>
+    </head>
+    <header class="header">
+        <div class="header-content">
+            <!-- <a class="logo"><img src="" alt="logo"></a> -->
+            <a class="logo">BUDOL!</a>
+            <form id="search" method="post" action="search.php"><input type="text" name="search" placeholder="SEARCH"></form>
+        </div>
+        <div class="header-content">
+            <nav>
+                <ul class="nav_links">
+                    <li><a href="landing.php">HOME</a></li>
+                </ul>
+            </nav>
+            <a href="login.php" class="user">LOG IN</a>
+        </div>
+    </header>
+        `
+        ;
+  }
+}
+
+customElements.define('header-component', Header);
---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BUDOL</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- LINKS -->
+        <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/home.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Amarante&family=Darker+Grotesque:wght@400;600;700;900&display=swap" rel="stylesheet">
+        <!-- COMPONENTS -->
+        <script src="components/header.js" type="text/javascript" defer></script>
+    </head>
+    <body>
+
+        <!-- NAVIGATION BAR -->
+        <header-component></header-component>
+
+        <!-- SHOP -->
+        <section class="content">
+            <!-- PRODUCTS DISPLAY -->
+            <div class="browsing-items">
+                <div class="row">
+                    <div class="listing">
+                        <img src="images/placeholder.png" class="placeholder">
+                        <h3>Product Name</h3>
+                        <p>PHP 100.00</p>
+                        <a href="" class="details-button">MORE DETAILS</a>
+                    </div>
+                    <div class="listing">
+                        <img src="images/placeholder.png" class="placeholder">
+                        <h3>Product Name</h3>
+                        <p>PHP 100.00</p>
+                        <a href="" class="details-button">MORE DETAILS</a>
+                    </div>
+                    <div class="listing">
+                        <img src="images/placeholder.png" class="placeholder">
+                        <h3>Product Name</h3>
+                        <p>PHP 100.00</p>
+                        <a href="" class="details-button">MORE DETAILS</a>
+                    </div>
+                    <div class="listing">
+                        <img src="images/placeholder.png" class="placeholder">
+                        <h3>Product Name</h3>
+                        <p>PHP 100.00</p>
+                        <a href="" class="details-button">MORE DETAILS</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="listing">
+                        <img src="images/placeholder.png" class="placeholder">
+                        <h3>Product Name</h3>
+                        <p>PHP 100.00</p>
+                        <a href="" class="details-button">MORE DETAILS</a>
+                    </div>
+                    <div class="listing">
+                        <img src="images/placeholder.png" class="placeholder">
+                        <h3>Product Name</h3>
+                        <p>PHP 100.00</p>
+                        <a href="" class="details-button">MORE DETAILS</a>
+                    </div>
+                    <div class="listing">
+                        <img src="images/placeholder.png" class="placeholder">
+                        <h3>Product Name</h3>
+                        <p>PHP 100.00</p>
+                        <a href="" class="details-button">MORE DETAILS</a>
+                    </div>
+                    <div class="listing">
+                        <img src="images/placeholder.png" class="placeholder">
+                        <h3>Product Name</h3>
+                        <p>PHP 100.00</p>
+                        <a href="" class="details-button">MORE DETAILS</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </body>
+</html>
