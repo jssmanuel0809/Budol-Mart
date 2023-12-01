85216a21f59a5ad925569291a7248a90a1ec0d41
mochi <maalyzahebora@gmail.com>
2023-12-02 04:04:03 +0800

    landing page 5%

---
+++ landing.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BUDOL</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- LINKS -->
+        <link rel="stylesheet" href="style/landing.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Righteous&family=VT323&display=swap" rel="stylesheet">
+        <!-- COMPONENTS -->
+        <script src="components/header.js" type="text/javascript" defer></script>
+        <script src="components/footer.js" type="text/javascript" defer></script>
+    </head>
+    <body>
+        <!-- Nav Bar -->
+        <header class="header">
+            <div class="header-content">
+                <li><a href="landing" class="logo"> BUDOL!</a></li>
+                <input type="text" id="search" name="search" placeholder="SEARCH">
+
+            </div>
+            <div class="links">
+                <nav>
+                        <ul class="nav_links">
+                        <li><a href="">PRODUCTS</a></li>
+                        <li><a href="">ABOUT</a></li>
+                    </ul>
+                </nav>
+                <a href="" class="login">LOG IN</a>
+            </div>
+        </header>
+
+        <!-- LANDING PAGE -->
+    </body>
