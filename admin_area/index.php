2dbf1df08b62e9bfd7269e8963e0083a4f031ff4
jemima <88172123+jssmanuel0809@users.noreply.github.com>
2023-12-01 15:42:40 +0800

    admin home

---
+++ admin_area/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BUDOL</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- LINKS -->
+        <link rel="stylesheet" href="../style/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Amarante&family=Darker+Grotesque:wght@400;600;700;900&display=swap" rel="stylesheet">
+        <!-- COMPONENTS -->
+        <script src="../components/admin_header.js" type="text/javascript" defer></script>
+    </head>
+    <body>
+
+        <!-- NAVIGATION BAR -->
+        <header-component></header-component>
+
+        <!-- SHOP -->
+        <!-- same lang class name ng section sa about, contact, index, tsaka products -->
+        <section class="content">
+
+        </section>
+
+    </body>
+</html>
