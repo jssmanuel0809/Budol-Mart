3730a03e1e23457d51d1a56359792ac1033f5a4f
mochi <maalyzahebora@gmail.com>
2023-12-04 02:17:20 +0800

    orders buyer and admin

--- components/orders.js
+++ components/orders.js
@@ -1,5 +1,3 @@
-//dapat maga-adjust yung box kada dagdag ng element
-
 document.getElementById('addOrderButton').addEventListener('click', function () {
     // Create a new row
     const newRow = document.createElement('div');
@@ -20,34 +18,3 @@ document.getElementById('addOrderButton').addEventListener('click', function () 
     const orderContents = document.querySelector('.order-box-contents');
     orderContents.appendChild(newRow);
 });
-
-//DROPDOWN
-document.addEventListener('DOMContentLoaded', function () {
-    const filterButton = document.getElementById('filterButton');
-    const dropdownContent = document.querySelector('.dropdown-content');
-    const filterOptions = document.querySelectorAll('.filter-option');
-    const currentFilterDisplay = document.getElementById('currentFilterDisplay');
-
-    // Handle filter button click
-    filterButton.addEventListener('click', function () {
-        dropdownContent.classList.toggle('show');
-    });
-
-    // Handle filter option click
-    filterOptions.forEach(option => {
-        option.addEventListener('click', function () {
-            const selectedValue = option.getAttribute('data-value');
-            currentFilterDisplay.textContent = `Filter: ${selectedValue.charAt(0).toUpperCase() + selectedValue.slice(1)}`;
-            console.log('Selected filter:', selectedValue);
-            dropdownContent.classList.remove('show'); // Close the dropdown after selecting an option
-        });
-    });
-
-    // Close the dropdown when clicking outside of it
-    window.addEventListener('click', function (event) {
-        if (!event.target.matches('.button')) {
-            dropdownContent.classList.remove('show');
-        }
-    });
-});
-
