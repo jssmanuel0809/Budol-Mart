ca4b0a749cc00b24cf20de89248124832f374cb2
mochi <maalyzahebora@gmail.com>
2023-12-03 15:57:48 +0800

    update

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="style/login.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Amarante&family=Darker+Grotesque:wght@400;600;700;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Righteous&family=VT323&display=swap" rel="stylesheet">
         <!-- COMPONENTS -->
         <script src="components/header.js" type="text/javascript" defer></script>
     </head>
--- orders.php
+++ orders.php
@@ -8,6 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- LINKS -->
         <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/orders.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Righteous&family=VT323&display=swap" rel="stylesheet">
@@ -17,5 +18,14 @@
 
     <body>
         <header-component></header-component>
+
+        <section class="qoute">
+            <h1>Orders</h1>
+            <div class="src-fltr">
+                <form id="search" method="post" action="search.php">
+                        <input type="text" name="search" placeholder="SEARCH">
+                </form>
+            </div>
+        </section>
     </body>
 </html>
