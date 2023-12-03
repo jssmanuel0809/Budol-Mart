081468168c0d301c79bc4adda2703ba361e9db07
mochi <maalyzahebora@gmail.com>
2023-12-03 13:30:30 +0800

    orders

--- components/admin_header.js
+++ components/admin_header.js
@@ -4,92 +4,96 @@ class Header extends HTMLElement {
     }
   
     connectedCallback() {
+      const currentPath = window.location.pathname;
+  
       this.innerHTML = `
-    <head>
-        <style>
-            header {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                background-color: #95A9CB;
-                padding-left: 20px;
-                height: 8vh;
-                z-index: 1;
-            }
-            .header{
-                width: 100%;
-                position: fixed;        
-                overflow: hidden;
-                top: 0;
-            }
-            .header-content{
-                display: flex;
-                justify-content: space-between;
-                align-items: cent
-            }
-            .logo{
-                font-family: 'Righteous', sans-serif;
-                color: #FCF1E6;
-                text-decoration: none;
-                letter-spacing: 10px;
-                font-size: 38px;
-                margin-right: 15px;
-            }
-            input[name="search"]{
-                width: 218px;
-                border: 2px solid #526C99;
-                border-radius: 25px;
-                background-color: #FFFFFF;
-                padding: 8px 20px;
-                margin: 8px 0;
-                opacity: 75%;
-            }
-            .nav_links a {
-                font-family: 'Dongle', sans-serif;
-                color: #3D4C6C;
-                text-decoration: none;
-            }
-            .nav_links {
-                list-style: none;
-                display: flex;
-                font-size: 40px;
-            }
-            .nav_links li {
-                padding: 5px 50px;
-            }
-            
-            .header-content .user{
-                background-color: #F6B185;
-                color: #FCF1E6;
-                text-decoration: none;
-                font-family: 'Dongle', sans-serif;
-                font-weight: bold;
-                font-size: 40px;
-                padding: 5px;
-                padding-left: 25px;
-                padding-right: 25px;
-            }
-        </style>
-    </head>
-    <header class="header">
-        <div class="header-content">
-           
-            <a href="index.php" class="logo">BUDOL!</a>
-            <form id="search" method="post" action="search.php"><input type="text" name="search" placeholder="SEARCH"></form>
-        </div>
-        <div class="header-content">
-            <nav>
-                <ul class="nav_links">
-                    <li><a href="products.php">PRODUCTS</a></li>
-                    <li><a href="orders.php">ORDERS</a></li>
-                </ul>
-            </nav>
-            <a href="logout.php" class="user">LOG OUT</a>
-        </div>
-    </header>
-        `
-        ;
+        <head>
+            <style>
+                header {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    background-color: #95A9CB;
+                    padding-left: 20px;
+                    height: 8vh;
+                    z-index: 1;
+                }
+                .header {
+                    width: 100%;
+                    position: fixed;
+                    overflow: hidden;
+                    top: 0;
+                }
+                .header-content {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center
+                }
+                .logo {
+                    font-family: 'Righteous', sans-serif;
+                    color: #FCF1E6;
+                    text-decoration: none;
+                    letter-spacing: 10px;
+                    font-size: 38px;
+                    margin-right: 15px;
+                }
+                input[name="search"] {
+                    width: 218px;
+                    border: 2px solid #526C99;
+                    border-radius: 25px;
+                    background-color: #FFFFFF;
+                    padding: 8px 20px;
+                    margin: 8px 0;
+                    opacity: 75%;
+                    display: ${currentPath.includes('orders.php') ? 'none' : 'block'};
+                }
+                .nav_links a {
+                    font-family: 'Dongle', sans-serif;
+                    color: #3D4C6C;
+                    text-decoration: none;
+                }
+                .nav_links {
+                    list-style: none;
+                    display: flex;
+                    font-size: 40px;
+                }
+                .nav_links li {
+                    padding: 5px 50px;
+                }
+  
+                .header-content .user {
+                    background-color: #F6B185;
+                    color: #FCF1E6;
+                    text-decoration: none;
+                    font-family: 'Dongle', sans-serif;
+                    font-weight: bold;
+                    font-size: 40px;
+                    padding: 5px;
+                    padding-left: 25px;
+                    padding-right: 25px;
+                }
+            </style>
+        </head>
+        <header class="header">
+            <div class="header-content">
+                <a href="index.php" class="logo">BUDOL!</a>
+                <form id="search" method="post" action="search.php">
+                    <input type="text" name="search" placeholder="SEARCH">
+                </form>
+            </div>
+            <div class="header-content">
+                <nav>
+                    <ul class="nav_links">
+                        <li><a href="products.php">PRODUCTS</a></li>
+                        <li><a href="orders.php">ORDERS</a></li>
+                    </ul>
+                </nav>
+                <a href="logout.php" class="user">LOG OUT</a>
+            </div>
+        </header>
+      `;
+    }
   }
-}
-
-customElements.define('header-component', Header);
+  
+  customElements.define('header-component', Header);
+  
---
+++ orders.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BUDOL Orders Buyer</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- LINKS -->
+        <link rel="stylesheet" href="style/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Righteous&family=VT323&display=swap" rel="stylesheet">
+
+        <script src="components/header.js" type="text/javascript" defer></script>
+    </head>
+
+    <body>
+        <header-component></header-component>
+    </body>
+</html>
