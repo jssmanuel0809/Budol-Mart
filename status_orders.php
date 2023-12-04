8e1d9eef5bf5d64dcf18c8447254b6335fa6b033
mochi <91691857+Mochicakes@users.noreply.github.com>
2023-12-04 23:12:39 +0800

    updates

--- status_orders.php
+++ status_orders.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Status | BUDOL Admin Portal</title>
+    <title>Status | BUDOL</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- LINKS -->
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Righteous&family=VT323&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="your-styles.css"> <!-- Add your custom styles -->
 
-    <script src="../components/status.js" type="text/javascript" defer></script>
+    <script src="/components/status.js" type="text/javascript" defer></script>
 </head>
 <body>
     <section class="box">
