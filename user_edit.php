3a18d9c7343f5457f005117d8bcdec7fbdd21ee2
aicla <airajpn06@gmail.com>
2023-12-05 00:14:50 +0800

    USER PROFILE

---
+++ user_edit.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BUDOL | Products</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- LINKS -->
+        <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/user_edit.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Righteous&family=VT323&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300;400;500;600;700;800;900&family=Righteous&display=swap" rel="stylesheet">
+        <!-- COMPONENTS -->
+        <script src="components/header.js" type="text/javascript" defer></script>
+        <script src="components/footer.js" type="text/javascript" defer></script>
+    </head>
+    <body>
+
+        <!-- NAVIGATION BAR -->
+        <header-component></header-component>
+
+        <section class="content">
+            <h1>USER PROFILE</h1>
+            <div class="form_box">
+                <form class="user_form">
+                    <h2>Username: Dummy</h2>
+                    <label for="">Name</label><input type="text" name="price" required>
+                    <label for="">Email</label><input type="text" name="stocks" required>
+                    <label for="">Contact #</label><input type="text" name="stocks" required>
+                    <label for="">Address Line 1</label><input type="text" name="stocks" required>
+                    <label for="">Address Line 2</label><input type="text" name="stocks" required>
+                    <button class="save_button" type="submit" name="edit_profile">Save</button>
+                </form>
+            </div>
+        </section>
+
+    </body>
+</html>
---
+++ user_profile.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BUDOL | Products</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- LINKS -->
+        <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/user_edit.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Righteous&family=VT323&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300;400;500;600;700;800;900&family=Righteous&display=swap" rel="stylesheet">
+        <!-- COMPONENTS -->
+        <script src="components/header.js" type="text/javascript" defer></script>
+        <script src="components/footer.js" type="text/javascript" defer></script>
+    </head>
+    <body>
+
+        <!-- NAVIGATION BAR -->
+        <header-component></header-component>
+
+        <section class="content">
+            <h1>USER PROFILE</h1>
+            <div class="form_box">
+                <div class="user_form">
+                    <h3>Username: Dummy</h3>
+                    <h3>Name: Dummy</h3>
+                    <h3>Email: D****2003@g****.com</h3>
+                    <h3>Contact #: 09*******67</h3>
+                    <h3>Address Line 1: Block 3 Lot 3, Dummy St., Dummy Subdivision</h3>
+                    <h3>Address Line 2: Barangay 111, Manila 1740, NCR, Philippines</h3>
+                    <button class="edit_button" type="submit" name="edit_profile">Edit</button>
+                </div>
+            </div>
+        </section>
+
+    </body>
+</html>
