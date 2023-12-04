40168aecc4f84defc7b0139ccfe78273b406cd85
aicla <airajpn06@gmail.com>
2023-12-04 17:44:16 +0800

    cart

--- admin_area/products.php
+++ admin_area/products.php
@@ -97,12 +97,12 @@
                         <td>true</td>
                         <td>
                             <a class="icon-button">
-                                <img src="product_images/edit.png" alt="Edit Icon" class="table-button">
+                                <img src="../images/edit.png" alt="Edit Icon" class="table-button">
                             </a>
                         </td>
                         <td>
                             <a class="icon-button" onclick="showUnlistPopup()">
-                                <img src="product_images/trash.png" alt="Unlist Icon" class="table-button">
+                                <img src="../images/trash.png" alt="Unlist Icon" class="table-button">
                             </a>
                         </td>
                     </tr>
