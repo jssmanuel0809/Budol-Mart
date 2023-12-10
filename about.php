18a78681b99e9591cc5d7fc0be5cf53a3e1821f4
mochi <91691857+Mochicakes@users.noreply.github.com>
2023-12-10 17:47:29 +0800

    about

--- about.php
+++ about.php
@@ -11,6 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- LINKS -->
         <link rel="stylesheet" href="style/style.css">
+        <link rel="stylesheet" href="style/about.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Righteous&family=VT323&display=swap" rel="stylesheet">
@@ -26,18 +27,16 @@
 
         <section class="content">
             <div class="box">
-                <p>Indulge in the captivating world of Budol, where<br>
-                    innovation meets excitement! Budol seamlessly<br> 
-                    blends the thrill of ecommerce with the charm of a<br>
-                    retail shop, offering a unique and delightful shopping<br>
-                    experience. Immerse yourself in the mystery and<br>
-                    anticipation as Budol specializes in selling blind<br> 
-                    boxes, each a treasure trove waiting to be<br> 
-                    discovered. Elevating your online shopping journey,<br> 
-                    our dedicated team ensures a seamless and efficient<br> 
-                    system for the shop, guaranteeing a hassle-free and<br> 
-                    enjoyable exploration of surprises. Unleash the joy of<br> 
-                    Budol and let the adventure unfold with every click!</p>
+                <p>Indulge in the captivating world of Budol, where innovation meets<br>
+                    excitement! Budol seamlessly blends the thrill of ecommerce with<br>
+                    the charm of a retail shop, offering a unique and delightful<br>
+                    shopping experience. Immerse yourself in the mystery and<br>
+                    anticipation as Budol specializes in selling blind boxes,<br> 
+                    discovered. Elevating your online shopping journey, our<br> 
+                    dedicated team ensures a seamless and efficient system for the<br> 
+                    shop, guaranteeing a hassle-free and enjoyable exploration of<br> 
+                    surprises. Unleash the joy of Budol and let the adventure unfold<br> 
+                    with every click!
                 
                     <div id="slider-container">
                     <img class="slider-image" src="images/img1.jpg" alt="Image 1">
@@ -51,6 +50,34 @@
             </div>
         </section>
 
+        <section class="lower">
+            <div class="image-container">
+                <div class="circular-image">
+                    <img src="images/aira.jpeg" alt="Image-first" class="Image-first">
+                </div>
+                <div class="image-text">
+                    <h3>Text for Image 1</h3>
+                </div>
+            </div>
+
+            <div class="image-container">
+                <div class="circular-image">
+                    <img src="images/jemi.jpg" alt="Image-middle">
+                </div>
+                <div class="image-text">
+                    <h3>Text for Image 2</h3>
+                </div>
+            </div>
+
+            <div class="image-container">
+                <div class="circular-image">
+                    <img src="images/aly.jpg" alt="Image-last">
+                </div>
+                <div class="image-text">
+                    <h3>Text for Image 3</h3>
+                </div>
+            </div>
+        </section>
         <!-- FOOTER -->
         <footer-component></footer-component>
     </body>
