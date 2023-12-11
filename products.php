0f85ac524578cd4020d3a6d6c162c7065396a414
aicla <airajpn06@gmail.com>
2023-12-11 19:43:46 +0800

    prodname

--- products.php
+++ products.php
@@ -1,7 +1,7 @@
 <?php 
     include('includes/server.php');
     // Number of products to display per page
-    $productsPerPage = 8;
+    $productsPerPage = 6;
 
     // Get the current page number from the URL
     $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
